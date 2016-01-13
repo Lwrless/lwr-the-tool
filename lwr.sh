197b6001cd17de4ca21917bbd5b824c1fba0f994
Lwrless <admin@lwrless.com>
2016-01-13 08:55:58 +0800

    Added become.

--- lwr.sh
+++ lwr.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 case "$1" in
+	become)
+		if [ "$2"a = a ]; then
+			echo "Usage: lwr become NAME"
+		else
+			sed 's/lwr/'$2'/g' /usr/local/bin/lwr > /usr/local/bin/$2
+			install /usr/local/bin/$2 /usr/local/bin/
+			rm /usr/local/bin/lwr
+		fi
+		;;
 	eat)
 		if [ "$2"a = a ]; then
 			echo "Usage: lwr eat FILE"
@@ -20,6 +29,6 @@ case "$1" in
 		rm /usr/local/bin/lwr
 		;;
 	*)
-		echo "Usage: lwr {eat|excrete|suicide}"
+		echo "Usage: lwr {become|eat|excrete|suicide}"
 		;;
 esac
